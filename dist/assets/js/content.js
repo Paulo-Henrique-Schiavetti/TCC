b8d7bee2f520dfb54ef98afb82e8891bfc0777ab
= <=>
2018-06-06 13:47:59 -0300

    algums estilos e correção de bugs

--- dist/assets/js/content.js
+++ dist/assets/js/content.js
@@ -41,7 +41,7 @@ function paginacadastrarusuario() {
     <div class="container">
         <ul class="center-align" id="cadastrodeprodutos">
             <input type="text" id="campoemail" placeholder="Email"/>
-            <input type="text" id="camposenha" placeholder="Senha"/>
+            <input type="password" id="camposenha" placeholder="Senha"/>
             <input type="text" id="camponome" placeholder="Nome"/>
             <input type="text" id="campotelefone" placeholder="__ ___ _ ____-____"/>
             <button onclick="cadastrarusuario()">Cadastrar</button>
