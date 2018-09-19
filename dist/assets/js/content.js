e09e15e3c98faf7393b4c18ba69c1809462c8b3c
ksamill <gabrielflopes2000@hotmail.com>
2018-09-18 21:47:51 -0300

    mudanças na pagina inicial

--- dist/assets/js/content.js
+++ dist/assets/js/content.js
@@ -10,35 +10,23 @@ function paginahome(){
     </div>
     `;
     let grid = document.querySelector("#grid");
-    //grid.addEventListener("click", clicaritem);
     page = 0;
+    pronta = true;
     exibiritens();
 }
 
 function item(id, locatario, nome, preco, descricao, imagem, endereco, estrelas) {
-    //abreviando endereço
-    localArray = endereco.split("");
-    localArray.length = 13;
-    localAbreviado = localArray.join("")+"...";
-    //abreviando nome
-    nomeArray = nome.split("");
-    if (nomeArray.length > 18){
-        nomeArray.length = 18;
-        nomeAbreviado = nomeArray.join("")+"...";
-    } else {
-        nomeAbreviado = nome;
-    }
     grid.innerHTML += `
     <div class="col s12 m4" data-id="${id}">
         <div class="card">
             <div class="card-image">
                 <img src="${imagem}" alt=""/>
                 <div class="card-content">
-                    <div class="card-nome" title="${nome}">${nomeAbreviado}</div>
-                    <div class="card-all" onclick="alugar(${id})"><a class="btn btnalugar">Allugar</a></div>
-                    <div class="card-add" onclick="add(${id})"><a class="btn btnadd"><i class="material-icons">shopping_cart</i></a></div>
+                    <div class="card-nome">${nome}</div>
+                    <div class="card-all"><a class="btn btnalugar">Allugar</a></div>
+                    <div class="card-add"><a class="btn btnadd"><i class="material-icons">shopping_cart</i></a></div>
                     <div class="card-livre"><p>LIVRE</p></div>
-                    <div class="card-local"><div title="${endereco}"><i class="material-icons">location_on</i>${localAbreviado}</div></div><br/>
+                    <div class="card-local"><i class="material-icons">location_on</i>${endereco}</div><br/>
                     <div class="card-estrela">${estrelas}</div>
                     <div class="card-preco">R$${preco}</div>
                 </div>
@@ -53,7 +41,7 @@ function paginacadastrarproduto() {
     <div class="container">
         <ul class="center-align">
             <form class="camponome">
-                <input type="text" id="camponome" placeholder="Nome"/>
+                <input type="text" id="nome" placeholder="Nome"/>
             </form> <br/>
             <form class="campopreço">
             <input type="text" id="campopreço" placeholder="Preço"/>
@@ -71,20 +59,21 @@ function paginacadastrarusuario() {
     header();
     content.innerHTML = `
     <div class="container">
-        <form class="emailcadastro" action="javascript:void(0);" onsubmit="cadastrarusuario()">
+        <form class="emailcadastro">
         <input type="text" id="campoemail" placeholder="Email"/>
         </form> <br/>
-        <form class="senhacadastro" action="javascript:void(0);" onsubmit="cadastrarusuario()">
+        <form class="senhacadastro">
             <input type="password" id="camposenha" placeholder="Senha"/>
         </form> <br/>
-        <form class="nomeusuario" action="javascript:void(0);" onsubmit="cadastrarusuario()">
+        <form class="nomeusuario">
             <input type="text" id="camponome" placeholder="Nome"/>
         </form> <br/>
-        <form class="telefone" action="javascript:void(0);" onsubmit="cadastrarusuario()">
+        <form class="telefone">
             <input type="text" id="campotelefone" placeholder="(DDD)97070-7070"/>
         </form> <br/>
-        <input type="file" id="campoimagem"/>
-        <button class="btn btncadastro" onclick="cadastrarusuario()">Cadastrar</button>
+        <form class="btn btncadastro"
+            <button onclick="cadastrarusuario()">Cadastrar</button>
+        </form>
     </div>
     `;
 }
@@ -93,13 +82,15 @@ function paginalogar() {
     content.innerHTML = `
     <div class="container">
         <ul class="center-align">
-        <form class="emaillogin" action="javascript:void(0);" onsubmit="validacao()">
+        <form class="emaillogin">
             <input type="text" id="email" placeholder="Email"/>
         </form> <br/>
-        <form class="senhalogin" action="javascript:void(0);" onsubmit="validacao()">
+        <form class="senhalogin">
             <input type="password" id="senha" placeholder="Senha"/>
         </form> <br/>
-            <button class="btn btnlogin" onclick="validacao()">login</button>
+        <form class="btn btnlogin"
+            <button onclick="validacao()">login</button>
+        </form>
         </ul>
     </div>
     `;
@@ -118,51 +109,4 @@ function paginaitem(id, item, endereco, imagem) {
     </div>
     `;
     exibircomentarios();
-}
-function paginaresultados(){
-    var texto = pesquisar.value;
-    header();
-    content.innerHTML =`
-    <div class="row" id="grid">
-    </div>
-    <div class="row">
-        <div class="vermais" onclick='exibiritens()'>
-            <a class="btn">Ver Mais</a>
-        </div>
-    </div>
-    `;
-    let grid = document.querySelector("#grid");
-    exibirresultados(texto);
-}
-function resultados(id, locatario, nome, preco, descricao, imagem, endereco, estrelas){
-    //abreviando endereço
-    localArray = endereco.split("");
-    localArray.length = 13;
-    localAbreviado = localArray.join("")+"...";
-    //abreviando nome
-    nomeArray = nome.split("");
-    if (nomeArray.length > 18){
-        nomeArray.length = 18;
-        nomeAbreviado = nomeArray.join("")+"...";
-    } else {
-        nomeAbreviado = nome;
-    }
-    grid.innerHTML += `
-    <div class="col s12 m4" data-id="${id}">
-        <div class="card">
-            <div class="card-image">
-                <img src="${imagem}" alt=""/>
-                <div class="card-content">
-                    <div class="card-nome" title="${nome}">${nomeAbreviado}</div>
-                    <div class="card-all" onclick="alugar(${id})"><a class="btn btnalugar">Allugar</a></div>
-                    <div class="card-add" onclick="add(${id})"><a class="btn btnadd"><i class="material-icons">shopping_cart</i></a></div>
-                    <div class="card-livre"><p>LIVRE</p></div>
-                    <div class="card-local"><div title="${endereco}"><i class="material-icons">location_on</i>${localAbreviado}</div></div><br/>
-                    <div class="card-estrela">${estrelas}</div>
-                    <div class="card-preco">R$${preco}</div>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
 }
